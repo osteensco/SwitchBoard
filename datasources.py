408628db98d05a505a8deeea140ebcecc4fd19fe
br0steen <osteensco@gmail.com>
2023-05-29 15:02:05 -0500

    init commit

---
+++ datasources.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import requests
+from google.cloud import bigquery
+import logging
+
+
+
+
+
+
+
+# parent objects
+
+class DataSource:
+    def __init__(self) -> None:
+        self.source = None
+        self.format = None
+        self.df = pd.DataFrame()
+        self.db_engine = None
+        self.table_name = None
+        self.dtypes = []
+        self.scheduled = None #Boolean flag used by pipeline to determine if data should be pulled or not
+        self.overwrite = None #date passed to Delete query for manually scheduled data pulls, avoids duplicate entries
+        self.APIkey = False
+        self.testitem = None
+        self.dataset = '''portfolio-project-353016.ALL.'''
+
+    def schedule(self):
+        '''
+        Method should include logic specific to child DataSource object.
+        '''
+        # used for determine if data should be ingested
+        # Returns True/False
+        self.db_engine = bigquery.Client('portfolio-project-353016')
+        # Child DataSource objects will have specific queries to determine the boolean value to return
+
+    def extract(self):
+        '''
+        Method should include logic specific to child DataSource object.
+        '''
+        return
+
+    def load(self):
+        if not self.schedule():#if manually scheduled
+            delete_tbl = f'''DELETE FROM `{self.dataset}{self.table_name}` WHERE Date = '{self.overwrite}' '''
+            self.db_engine.query(delete_tbl)
+            logging.info(f'Removed any duplicate data from {self.table_name}, table cleaned for landing new pull')
+        #land in appropriate tables
+        loadjob = bigquery.LoadJobConfig(schema=self.dtypes)
+        loadjob.write_disposition = 'WRITE_APPEND'
+        loadjob.schema_update_options = [bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION]
+        self.db_engine.load_table_from_dataframe(self.df, f'''{self.dataset}{self.table_name}''', loadjob).result()
+        logging.info(f'''{type(self).__name__} loaded into {self.table_name}''' )
+
+    def truncate(self):
+        query = f'''TRUNCATE TABLE {self.dataset}{self.table_name}'''
+        result = self.db_engine.query(query).result()
+        logging.info(f'''{type(self).__name__} - {result}''')
+
+    def getreq(self, url):
+        r = requests.get(url=url, 
+        headers={'User-Agent': '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36''','referer':'https://www.google.com/'})
+        try:
+            r.raise_for_status()
+        except Exception as ex:
+            logging.error(getattr(ex, 'message', repr(ex)))
+        return r
+
+    def test(self):
+        self.schedule()
+        self.extract()
+        if self.df.shape[0] > 0:
+            print(self.df.head()) 
+        elif self.testitem:
+            print(self.testitem)
+        else:
+            print('testitem/df failed to generate')
+
+
+class Query():
+    def __init__(self, sql_string) -> None:
+        self.body = sql_string
+        self.db_engine = None #db client object init in schedule method
+        self.scheduled = None #Boolean flag used by pipeline to determine if data should be pulled or not
+
+    def schedule(self):
+        '''
+        Method should include logic specific to child Query object.
+        '''
+        self.db_engine = bigquery.Client('portfolio-project-353016')
+
+    def run(self):
+        self.db_engine.query(self.body).result()
+
+
+
+
+
+
