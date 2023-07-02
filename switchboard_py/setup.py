10c8198cd3145fc8a08d2d706687530197b0d9bf
br0steen <osteensco@gmail.com>
2023-07-02 11:50:15 -0400

    added setup.py

---
+++ switchboard_py/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='switchboard-py',
+    version='1.0.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'switchboard-cli=switchboard_py.cli:main',
+        ],
+    }
+)
