3e8011e6e9f8517bd4f1862f240933e144a52ce1
br0steen <osteensco@gmail.com>
2023-07-02 12:07:01 -0400

    updated package name

--- switchboard_py/setup.py
+++ switchboard_py/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='switchboard-py',
+    name='cloud-switchboard',
     version='1.0.0',
     packages=find_packages(),
     entry_points={
