f35104b6b9011de3b19a0e4021886d87acd4a083
br0steen <osteensco@gmail.com>
2023-07-02 17:11:36 -0400

    debugging tweaks

--- switchboard_py/cli.py
+++ switchboard_py/cli.py
@@ -25,8 +25,8 @@ def main():
     subparsers = parser.add_subparsers(dest='command')
 
     start_project_parser = subparsers.add_parser('start_project', help='Create a new project')
-    start_project_parser.add_argument('project_name', type=str, required=True, help='Name of the project')
-    start_project_parser.add_argument('cloud_provider', type=str, required=True, action='ValidateArg', help='Cloud Provider being used')
+    start_project_parser.add_argument('project_name', type=str, help='Name of the project')
+    start_project_parser.add_argument('cloud_provider', type=str, action='ValidateArg', help='Cloud Provider being used')
 
     args = parser.parse_args()
 
