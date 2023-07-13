08d0b3f1d5b32dc968b294120809439d1b4d180f
br0steen <osteensco@gmail.com>
2023-07-13 13:14:59 -0500

    updated cloud_providers for switchboard_py api

--- switchboard_py/__init__.py
+++ switchboard_py/__init__.py
@@ -1,5 +1,5 @@
 from .utils import http_trigger, connect_to_bucket, init_log
-from .cloud_types import GCP
+from .cloud_providers import GCP
 from .caller import Caller
 from .switchboard import SwitchBoard
 
